072eefa7afacc7981c3bef3486b675351f4d6bc0
PavelM2205 <pavelm2205@mail.ru>
2022-02-01 16:03:09 +0700

    6.4.1. Сравнить последние элементы двух массивов [#304228]

---
+++ src/test/java/ru/job4j/array/EqLastTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EqLastTest {
+
+    @Test
+    public void whenEq() {
+        int[] left = {1, 2, 3};
+        int[] right = {5, 4, 3};
+        boolean out = EqLast.check(left, right);
+        Assert.assertTrue(out);
+    }
+
+    @Test
+    public void whenNotEq() {
+        int[] left = {1, 2, 3};
+        int[] right = {3, 3, 4};
+        boolean out = EqLast.check(left, right);
+        Assert.assertFalse(out);
+    }
+}
