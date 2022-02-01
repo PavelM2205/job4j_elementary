891e935de0a7074e7cbbc6076e3ec1f564bd0084
PavelM2205 <pavelm2205@mail.ru>
2022-02-01 09:42:06 +0700

    5.7. Ипотека [#156315 #258243]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,9 +4,8 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int count = 0;
         while (amount > 0) {
-            double debt = ((amount * percent) / 100) + amount;
-            amount = debt - salary;
-            count += 1;
+            amount = ((amount * percent) / 100) + amount - salary;
+            count++;
         }
         return count;
     }
