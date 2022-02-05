1587df7865afe5445ebb21e44a8411ae466d014f
PavelM2205 <pavelm2205@mail.ru>
2022-02-05 12:59:53 +0700

    3. Перегрузить метод max для трех чисел [#122644]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -33,4 +33,98 @@ public class MaxTest {
         int out = Max.max(first, second);
         Assert.assertEquals(expexted, out);
     }
+
+    @Test
+    public void when234Then4() {
+        int first = 2;
+        int second = 3;
+        int third = 4;
+        int expexted = 4;
+        int out = Max.max(first, second, third);
+        Assert.assertEquals(expexted, out);
+    }
+
+    @Test
+    public void when243Then4() {
+        int first = 2;
+        int second = 4;
+        int third = 3;
+        int expexted = 4;
+        int out = Max.max(first, second, third);
+        Assert.assertEquals(expexted, out);
+    }
+
+    @Test
+    public void when423Then4() {
+        int first = 4;
+        int second = 2;
+        int third = 3;
+        int expexted = 4;
+        int out = Max.max(first, second, third);
+        Assert.assertEquals(expexted, out);
+    }
+
+    @Test
+    public void when443Then4() {
+        int first = 4;
+        int second = 4;
+        int third = 3;
+        int expexted = 4;
+        int out = Max.max(first, second, third);
+        Assert.assertEquals(expexted, out);
+    }
+
+    @Test
+    public void when444Then4() {
+        int first = 4;
+        int second = 4;
+        int third = 4;
+        int expexted = 4;
+        int out = Max.max(first, second, third);
+        Assert.assertEquals(expexted, out);
+    }
+
+    @Test
+    public void when4444Then4() {
+        int first = 4;
+        int second = 4;
+        int third = 4;
+        int fourth = 4;
+        int expexted = 4;
+        int out = Max.max(first, second, third, fourth);
+        Assert.assertEquals(expexted, out);
+    }
+
+    @Test
+    public void when1235Then5() {
+        int first = 1;
+        int second = 2;
+        int third = 3;
+        int fourth = 5;
+        int expexted = 5;
+        int out = Max.max(first, second, third, fourth);
+        Assert.assertEquals(expexted, out);
+    }
+
+    @Test
+    public void when1532Then5() {
+        int first = 1;
+        int second = 5;
+        int third = 3;
+        int fourth = 2;
+        int expexted = 5;
+        int out = Max.max(first, second, third, fourth);
+        Assert.assertEquals(expexted, out);
+    }
+
+    @Test
+    public void when5123Then5() {
+        int first = 5;
+        int second = 1;
+        int third = 2;
+        int fourth = 3;
+        int expexted = 5;
+        int out = Max.max(first, second, third, fourth);
+        Assert.assertEquals(expexted, out);
+    }
 }
